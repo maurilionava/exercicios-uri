5dbba30e61c8ccb6e40bdbce33c9e21b0f65d86c
Maurilio Nava Junior <mnavajunior@gmail.com>
2022-11-10 16:20:48 -0300

    Criada resolução exercício 1001 iniciante C#

---
+++ Beginner/1001-Extremely Basic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int X = A + B;
+
+        Console.WriteLine("X = {0}", X);
+    }
+}
