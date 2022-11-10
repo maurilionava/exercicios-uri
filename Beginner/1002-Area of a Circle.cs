e0ff5627c032164a195189251ddec0937c254d42
Maurilio Nava Junior <mnavajunior@gmail.com>
2022-11-10 16:21:41 -0300

    Criada resolução do exercício 1002 iniciante C#

---
+++ Beginner/1002-Area of a Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double R = Convert.ToDouble(Console.ReadLine());
+
+        double A = 3.14159 * (R * R);
+
+        Console.WriteLine("A={0}", A.ToString("0.0000"));
+    }
+}
